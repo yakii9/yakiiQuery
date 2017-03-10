3f31752f8f0d0f721b900528ac3881fd2b680ef5
段诗闻 <yakii9@vip.qq.com>
2017-03-10 16:15:43 +0800

    添加分享单页框架的基础代码（包含处理GET请求、操作DOM的基础代码）

---
+++ src/single/index.js
@@ -0,0 +1,92 @@
+(function () {
+  // 存储一些基础信息
+  var id = localStorage.getItem("userId");
+  var token = localStorage.getItem("token");
+  var APIs = {
+
+  };
+
+  // 处理网络请求和取出本地记录、消除loading状态等等的基础函数
+  var infrastructure = {
+    handleGetRequest: function(url, callback, authToken) {
+      var httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          callback(httpRequest.response);
+        }
+      }
+      httpRequest.open('GET', url);
+      httpRequest.setRequestHeader("auth-token", authToken);
+      httpRequest.send();
+
+      return httpRequest
+    },
+    removeLoading: function() {
+      var loading = document.getElementById('loading');
+      loading.style.opacity = 1;
+      var opaqueTimer = setInterval(function () {
+        loading.style.opacity = loading.style.opacity - 0.025;
+      }, 50);
+
+      setTimeout(function () {
+        clearInterval(opaqueTimer);
+      }, 2000);
+    },
+    accumulator: function(initValue, stepValue) {
+      stepValue = stepValue || 1;
+      initValue = initValue || 0;
+
+      var value = initValue;
+
+      return {
+        add: function() {
+          value = value + stepValue;
+        },
+        isSuccess: function(bigEnough) {
+          return Boolean(value >= bigEnough);
+        }
+      }
+    },
+    setContentById: function(id, content, type) {
+      type = type || "innerHTML";
+      var elem = document.getElementById(id);
+      if (!elem) return
+      elem[type] = content;
+    },
+    setCssById: function(id, rule, type) {
+      var elem = document.getElementById(id);
+      if (!elem) return
+      elem.style[type] = rule;
+    },
+    caculateAge: function(birthDay) {
+      if (!birthDay) return ""
+
+      birthDay = new Date(birthDay)
+      var now = new Date()
+
+      if (now.getUTCFullYear() - birthDay.getUTCFullYear() === 0) {
+        var month = (now.getMonth() - birthDay.getMonth())
+        if (month === 0) {
+          return (now.getDate() - birthDay.getDate()) + "天"
+        } else {
+          return month + "个月"
+        }
+      }
+
+      return (now.getUTCFullYear() - birthDay.getUTCFullYear()) + "岁"
+    }
+  }
+
+  // 业务逻辑相关的代码
+  var business = {
+    addContent: function() {
+
+    }
+  }
+
+  var finishLoadingEvent = new Event('finishLoading');
+
+  business.addContent()
+
+  document.body.addEventListener('finishLoading', infrastructure.removeLoading, false);
+}())
