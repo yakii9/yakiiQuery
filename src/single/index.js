2ac2a8ecb2c8984415ddc253c8c3239e700408b6
Yakii Duan <yakii9@vip.qq.com>
2017-05-10 12:34:03 +0800

    增加将数组插入DOM的函数

--- src/single/index.js
+++ src/single/index.js
@@ -3,6 +3,32 @@
     var loadingTimer = 0;
 
     var infrastructure = {
+        buildElementsTree: function(entity) {
+            if (!entity) return null;
+
+            var i, j;
+            if (Array.isArray(entity)) {
+              // 我们约定，数组的第一个元素存储根节点的信息
+              var root = infrastructure.buildElementsTree(entity[0]);
+              for (i=1; i<entity.length; i++) {
+                  root.appendChild(infrastructure.buildElementsTree(entity[i]));
+              }
+              return root;
+            } else {
+                var element = document.createElement(entity.targetName);
+                var properties = Object.getOwnPropertyNames(entity);
+                for (j=0; j<properties.length; j++) {
+                    element[properties[j]] = entity[properties[j]];
+                }
+                return element;
+            }
+        },
+        insertElementsArray: function(list, targetNodeId) {
+            if (!list || !targetNodeId) return
+
+            var root = infrastructure.buildElementsTree(list);
+            return document.getElementById(targetNodeId).appendChild(root);
+        },
         setContentById: function(id, content, type) {
             type = type || "innerHTML";
             var elem = document.getElementById(id);
